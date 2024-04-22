3f0e65342326c5bc0b827b725cb9eb819f6e65c7
drnisar <65063665+drnisar@users.noreply.github.com>
2024-04-22 09:03:16 +0500

    Content of open surgery card changed

--- src/pagesContent/Content.tsx
+++ src/pagesContent/Content.tsx
@@ -8,7 +8,7 @@ export const CardContent = [
   {
     title: "Open Surgery",
     detail:
-      "A variety of general surgical operations are offered at our clinic. Our special interest is in hernia surgery, hemorrhoidal diseases and other gastrointestinal procedures",
+      "A variety of general surgical operations are offered at our clinic. Our special interest is in hernia surgery, hemorrhoidal diseases and other gastrointestinal procedures. We offer day care services as well",
     image: "./assets/openSurgeryPhoto.jpeg",
   },
   {
